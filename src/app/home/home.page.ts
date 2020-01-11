b6672c9d7ec4888cfccb16b18b667d511da78624
Alexandra O <alexandra.olivares.solis@gmail.com>
2020-01-11 14:27:12 -0300

    ADD: Se sube el proyecto

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+
+export class HomePage {
+
+  public direccion_salida: string;
+  public direccion_llegada: string;
+
+  constructor(private alertController: AlertController,
+              private router: Router,) {}
+
+  buttonClick(){
+  alert(this.direccion_salida)
+  this.router.navigate(['/list']);
+  }
+}
